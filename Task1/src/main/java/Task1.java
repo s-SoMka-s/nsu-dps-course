30fc4bb1a06b676b72bef1bbabfe4b17e536b7da
i.chiesov <i.chiesov@gmail.com>
2021-10-14 21:32:26 +0700

    Rewrite task1.

--- Task1/src/main/java/Task1.java
+++ Task1/src/main/java/Task1.java
@@ -1,12 +1,13 @@
 public class Task1 {
   public static void main(String[] args) {
-    new Thread(Task1::printSomething).start();
-    printSomething();
+    Runnable task = () -> printSomething("child");
+    new Thread(task).start();
+    printSomething("parent");
   }
 
-  private static void printSomething() {
+  private static void printSomething(String caller) {
     for (var i = 0; i < 10; i++){
-      System.out.println((i+1) + ": string");
+      System.out.println(caller + " prints " +  (i+1) + ": string");
     }
   }
 }
