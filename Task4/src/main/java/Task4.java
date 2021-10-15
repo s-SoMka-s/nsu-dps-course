a073701f6c11c1d5f578b61c19fce98678c25623
i.chiesov <i.chiesov@gmail.com>
2021-10-15 21:22:40 +0700

    rewrite task4

--- Task4/src/main/java/Task4.java
+++ Task4/src/main/java/Task4.java
@@ -1,16 +1,17 @@
+import java.util.concurrent.TimeUnit;
+
 public class Task4 {
-  public static void main(String[] args) {
-    var milliSecToWait = 2 * 1000;
+  public static void main(String[] args) throws InterruptedException {
     var child = new Thread(() -> {
-      try {
-        System.out.println("I was started");
-        Thread.sleep(milliSecToWait);
-      } catch (InterruptedException e) {
-        System.out.println("I was interrupted");
+      while(!Thread.currentThread().isInterrupted()) {
+        System.out.println("I am working...");
       }
+
+      System.out.println("I was interrupted(");
     });
 
     child.start();
+    TimeUnit.SECONDS.sleep(2);
     child.interrupt();
   }
 }
