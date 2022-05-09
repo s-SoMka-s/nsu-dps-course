48ac2c206014251a75b05dd0679cf02476e4dc26
i.chiesov <i.chiesov@gmail.com>
2022-05-09 20:30:49 +0700

    move xml

--- XML/Task1/src/main/java/task1/XmlElementAttributeNames.java
+++
@@ -1,7 +0,0 @@
-package task1;
-
-public class XmlElementAttributeNames {
-  public static final String Id = "id";
-  public static final String Name = "name";
-  public static final String Value = "value";
-}
