f99755293acde64a1860f53fd559abdec72f4063
Igor Chiesov <32841655+s-SoMka-s@users.noreply.github.com>
2022-05-17 09:45:50 +0700

    fix d4

--- D4/script.sql
+++ D4/script.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS flight_price;
 CREATE TABLE flight_price AS (
-    SELECT departure_airport, arrival_airport, fare_conditions, AVG(amount) amount FROM ticket_flights tf
-    -- inner join
+    SELECT departure_airport, arrival_airport, fare_conditions, seat_no, avg(amount) amount FROM ticket_flights tf
     JOIN flights fl ON tf.flight_id = fl.flight_id
-    GROUP BY fare_conditions, departure_airport, arrival_airport
+    JOIN boarding_passes bp on tf.ticket_no = bp.ticket_no and tf.flight_id = bp.flight_id
+    GROUP BY departure_airport, arrival_airport, fare_conditions, seat_no
 );
